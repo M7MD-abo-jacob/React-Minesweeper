009ce83ba2266f651a05237e177b8163e21d3772
M7MD-abo-jacob <m7md.master1@gmail.com>
2023-04-20 15:44:11 +0300

    optimized setMines function

--- src/lib/setMines.js
+++ src/lib/setMines.js
@@ -3,23 +3,32 @@
 // because you don't wanna lose on your first click, do you?
 
 export default function setMines(tileId, width, minesCount) {
-  const tileR = tileId.split("-")[0];
-  const tileC = tileId.split("-")[1];
-  let thisId = `${tileR}${tileC}`;
+  // id of the tile that the user clicked
+  const clickedId = [tileId.split("-")[0], tileId.split("-")[1]];
+
+  // to keep track of mines and return them at the end
   const minesLocations = [];
-  const testMinesLocs = [];
+
+  // to keep track of number of mines
   let minesLeft = minesCount;
   // keeps looping until the number is satisfied
   while (minesLeft > 0) {
-    let r = Math.floor(Math.random() * width).toString();
-    let c = Math.floor(Math.random() * width).toString();
-    let id = [r, c];
+    // create random tile for a mine to check
+    const r = Math.floor(Math.random() * width).toString();
+    const c = Math.floor(Math.random() * width).toString();
+    const testId = [r, c];
 
-    let testId = `${r}${c}`;
-
-    if (!testMinesLocs.includes(`${r}${c}`) && testId !== thisId) {
-      minesLocations.push(id);
-      testMinesLocs.push(`${r}${c}`);
+    // check if this mine already exists
+    // also check if this is the first tile the player clicked
+    // no one wants to lose on first click ;)
+    // other than that, set the mine location
+    if (
+      !JSON.stringify(JSON.parse(JSON.stringify(minesLocations))).includes(
+        JSON.stringify(testId)
+      ) &&
+      JSON.stringify(testId) !== JSON.stringify(clickedId)
+    ) {
+      minesLocations.push(testId);
       minesLeft--;
     }
   }
